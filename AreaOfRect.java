0cac85d03b57191092c94a39589f53b3acbdfecf
adeebrk <imadeebrk@gmail.com>
2024-01-03 12:21:34 +0530

    This is my first git repo

---
+++ AreaOfRect.java
@@ -0,0 +1,11 @@
+
+public class AreaOfRect {
+	
+public static void main (String[] args)
+{
+int width=5;
+int height=10;
+int area=width*height;
+System.out.println("Area of rectangle="+area);
+}
+}
---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=13;
+if (num%2==0)
+{
+	System.out.println("Number is Even");
+	
+}
+else
+{
+	System.out.println("Number is Odd");
+}
+	}
+
+}
